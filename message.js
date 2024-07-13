d3b83cc17261de14ac2cd6ea25091b8b95b2495a
Carlojead <amaquincj00@gmail.com>
2024-07-13 11:33:41 +0800

    Js module sample

--- message.js
+++ message.js
@@ -2,7 +2,7 @@ const message = () => {
     const name = "Carlo";
     const age = 21;
 
-    return `${name} is ${age} years old`;
+    return `${name} is ${age} years old nga gwapo`;
 };
 
 export default message;
