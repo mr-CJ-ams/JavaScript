6af942715e31455fd27a9cc9eae55d0cc567d31a
Carlojead <amaquincj00@gmail.com>
2024-07-13 11:29:04 +0800

    Initial commit

---
+++ message.js
@@ -0,0 +1,8 @@
+const message = () => {
+    const name = "Carlo";
+    const age = 21;
+
+    return `${name} is ${age} years old`;
+};
+
+export default message;
---
+++ script.js
@@ -0,0 +1,2 @@
+import message from "./message.js";    
+document.getElementById("demo").innerHTML = message();
