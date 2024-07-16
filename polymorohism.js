aa384e1b419c10ac9be5bc7e6ba4572c2f863c70
Carlojead <amaquincj00@gmail.com>
2024-07-16 13:14:39 +0800

    Inheretance and Polymorphism

---
+++ polymorohism.js
@@ -0,0 +1,36 @@
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    sayName(){
+        console.log(this.name);
+    }
+}
+
+class Student extends Person{
+    constructor(name, age, rollNumber){
+        super(name,age);
+        this.rollNumber = rollNumber;
+    }
+
+    logDetails(){
+        console.log(`Name: ${this.name}, Age: ${this.age}, Roll number: ${this.rollNumber}`)
+    }
+
+    sayName(){
+        console.log("Student 1");
+        super.sayName();
+    }
+
+
+}
+
+const student = new Student("Carlo", 22, 1);
+
+student.logDetails();
+student.sayName();
+
+const person = new Person("Jead", 25, 2);
+person.sayName();
